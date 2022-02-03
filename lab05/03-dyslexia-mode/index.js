160769e0b5ce00be7b2eca5c8e00a9d74e84f80f
candytanti <candytanti5@gmail.com>
2022-02-03 11:35:58 -0600

    Commiting my completed lab05 files

---
+++ lab05/03-dyslexia-mode/index.js
@@ -0,0 +1,32 @@
+/* 
+  See Smashing Magazine Tutorial:
+  https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
+*/
+
+const initPage = () => {
+  // toggle dyslexia support
+  const isPressed = window.localStorage.getItem('dyslexic') === true;
+
+  if (isPressed){
+    document.body.classList.add('dyslexia-mode');
+  }
+
+  const toggle = document.getElementById('dyslexia-toggle');
+
+  if (isPressed) {
+    toggle.setAttribute('aria-pressed', 'true');
+  }
+
+  toggle.addEventListener('click', toggleEventHandler);
+}
+
+const toggleEventHandler = ev => {
+  let pressed = ev.currentTarget.getAttribute('aria-pressed') === true;
+
+  ev.currentTarget.setAttribute('aria-pressed', String(!pressed));
+  document.body.classList.toggle("dyslexia-mode");
+  window.localStorage.setItem("dyslexia", String(!pressed));
+} 
+
+initPage();
+  
